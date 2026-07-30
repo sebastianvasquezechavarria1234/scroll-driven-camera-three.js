51a458da1a410ed845ff5da3331e5cf5917cc9f5
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-07-30 08:40:05 -0500

    Fix: update Lenis configuration to use lerp for smoother scrolling

--- main.js
+++ main.js
@@ -15,8 +15,7 @@ const clock = new THREE.Clock();
 const container = document.getElementById( 'container' );
 
 const lenis = new Lenis({
-	duration: 1.2,
-	easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+	lerp: 0.08,
 	smoothWheel: true,
 });
 
